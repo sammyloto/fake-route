15e1f1e81daef3b82e9588f153061ab7839d2992
Leroy <samuel.gacho@gmail.com>
2019-09-19 17:08:53 +0300

    added socket connection, plus testing script on index.html

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [2, 4],
+        "import/no-extraneous-dependencies": [2, { "devDependencies": true }]
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 const app = require('express')();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', async (req, res) => {
-    res.sendFile(`${__dirname}/views/index.html`);
-});
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected');
-//     });
-// });
-
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        // eslint-disable-next-line no-console
-        console.log(`message: ${msg}`);
-    });
+    console.log('a user connected');
 });
 
 
-const server = app.listen(3000, () => {
+app.get('/fakeroute', async (req, res) => {
+    const locations = [
+        { locationOne: [1.289984, 36.783166] },
+        { locationTwo: [1.290928, 36.783271] },
+        { locationThree: [1.291744, 36.783410] },
+        { locationFour: [1.292403, 36.783480] },
+        { locationFive: [1.292344, 36.784247] },
+        { locationSix: [1.292280, 36.785513] },
+    ];
+
+    const timeout = 3000;
+
+    const done = locations.forEach((item, index) => {
+        // eslint-disable-next-line
+        setTimeout(() => {
+            io.emit('route', item, index);
+            return (item, index);
+        }, index * timeout);
+    });
+    res.send(done);
+});
+
+
+const server = http.listen(3000, () => {
     // eslint-disable-next-line no-console
     console.log('listening on *:3000');
 });
